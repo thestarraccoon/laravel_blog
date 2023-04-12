09c517cbe92a98348e7ae36ac29b3b972d877431
Maxim Chekulaev <thestarraccoon@gmail.com>
2023-04-13 01:20:40 +0400

    Category create form

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,8 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'],
     });
 
     Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
-        Route::get('/', 'IndexController')->name('categories.index');
+        Route::get('/', 'IndexController')->name('admin.category.index');
+        Route::get('/create', 'CreateController')->name('admin.category.create');
     });
 });
 
