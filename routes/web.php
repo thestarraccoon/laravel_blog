8c93077454c0e0df0903631141a0f79f45b67b69
Maxim Chekulaev <thestarraccoon@gmail.com>
2023-04-13 00:54:04 +0400

    INIT

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+
+use App\Http\Controllers\Main\IndexController;
+use App\Http\Controllers\Admin\Main\AdminIndexController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', 'AdminIndexController')->name('admin.index');
+    });
+
+    Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
+        Route::get('/', 'IndexController')->name('categories.index');
+    });
+});
+
+
+Auth::routes();
+
+
